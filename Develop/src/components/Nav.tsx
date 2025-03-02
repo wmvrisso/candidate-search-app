df170f13cd646c11f6da84591e3ea9190e581442
wmvrisso <wmvrisso@gmail.com>
2025-03-02 14:15:39 -0800

    added nav bar

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,13 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <h1>Navigation Bar</h1>
+      <ul>
+        <li><a href="/">Candidate Search</a></li>
+        <li><a href="/savedcandidates">Saved Candidates</a></li>
+      </ul>
+    </div>
   )
 };
 
